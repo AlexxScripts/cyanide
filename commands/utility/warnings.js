b64224ef64c87ad8d6a132bcbaf5d3d70ced422a
AlexxScripts <78374927+AlexxScripts@users.noreply.github.com>
2021-10-14 23:54:42 +0800

    Create warnings.js

---
+++ commands/utility/warnings.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js')
+const db = require('../../models/warningdb')
+const { version, prefix } = require('../../config.json')
+
+module.exports = {
+    name: 'warn',
+    usage: `${prefix}warn [user] {reason}`,
+    aliases: [],
+    inProgress: true,
+    involvesModeration: true,
+    permission: 'MANAGE_MESSAGES',
+    category: 'MODERATION',
+    cooldown: 0,
+    description: 'Gives a warning to a user.',
+
+    async run(bot, message, args){
+
+        let target = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        let target2
+        let thisuserhas
+        if(!target){
+            target = message.author
+            target2 = target
+            thisuserhas = `${target} has`
+        } else {
+            target2 = target.user
+            thisuserhas = `You have`
+        }
+
+        db.findOne({ GuildID: message.guild.id, UserID: target.id, UserTag: target.user.tag }, async (err, data) => {
+            if(err) throw err
+            if(data){
+
+                const warningsEmbed = new Discord.MessageEmbed()
+                .setAuthor(bot.user.username, bot.user.displayAvatarURL())
+                .setTitle(`${target2.tag}'s '`)
+                .setDescription(`${data.Content.map(
+                    (w, i) => `Warn ID: ${i + 1}\nModerator: ${w.ModeratorTag}\nReason: ${w.Reason}\nDate: ${w.Date}\n`
+                )}`).join(' ')
+
+                message.channel.send({ embeds: [warningsEmbed] })
+            } else {
+
+                const warningsEmbed2 = new Discord.MessageEmbed()
+                .setAuthor(bot.user.username, bot.user.displayAvatarURL())
+                .setTitle(`${thisuserhas} no warnings.`)
+                message.channel.send({ embeds: [warningsEmbed2] })
+            }
+        })
+    }
+}
