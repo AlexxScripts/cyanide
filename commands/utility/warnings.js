53d93b0d90ffe074e301c3344e6c25cf6c828464
AlexxScripts <78374927+AlexxScripts@users.noreply.github.com>
2021-10-15 00:13:49 +0800

    Update warnings.js

--- commands/utility/warnings.js
+++ commands/utility/warnings.js
@@ -44,7 +44,7 @@ module.exports = {
                 .setAuthor(bot.user.username, bot.user.displayAvatarURL())
                 .setTitle(`${target3} Warnings'`)
                 .setDescription(`${data.Content.map(
-                    (w, i) => `Warn ID: ${i + 1}\nModerator: ${w.ModeratorTag}\nReason: ${w.Reason}\nDate: ${w.Date}\n`
+                    (w, i) => `Warn ID: ${i + 1}\nModerator: ${w.ModeratorTag}\nReason: ${w.Reason}\nDate: ${w.Date}\n\n`
                 ).join(' ')}`)
 
                 message.channel.send({ embeds: [warningsEmbed] })
