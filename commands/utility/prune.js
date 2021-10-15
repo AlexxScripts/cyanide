187ef683b278a8ed874352381bb68bcdec42994d
AlexxScripts <78374927+AlexxScripts@users.noreply.github.com>
2021-10-15 14:34:35 +0800

    Update prune.js

--- commands/utility/prune.js
+++ commands/utility/prune.js
@@ -15,7 +15,7 @@ module.exports = {
     async run (bot, message, args){
 
         let amount = parseInt(message.content.split(' ')[1])
-        const messages = message.channel.messages.fetch({ limit: amount + 1 })
+        const messages = message.channel.messages.fetch({ limit: amount })
 
         const noAmountEmbed = new Discord.MessageEmbed()
         .setAuthor(bot.user.username, bot.user.displayAvatarURL())
@@ -25,6 +25,7 @@ module.exports = {
 
         if(!amount) return message.channel.send({ embeds: [noAmountEmbed] })
         if(amount > 100) amount = 100
+        await message.delete()
 
         const deletedEmbed = new Discord.MessageEmbed()
         .setAuthor(bot.user.username, bot.user.displayAvatarURL())
