2fbb0aba75e72030581c20ae6c0ec90aca410137
AlexxScripts <78374927+AlexxScripts@users.noreply.github.com>
2021-10-15 21:44:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ bot.on('messageCreate', async message => {
         if(command){
 
             if(command.category == 'SPECIAL'){
-                if(message.author.id == '705014771694567475' || '514720382779916318'){} else {
+                if(message.author.id != '514720382779916318'){
                     const ownerOnlyEmbed = new Discord.MessageEmbed()
                     .setAuthor(bot.user.username, bot.user.displayAvatarURL())
                     .addField('Owners Only!', `Only Amy or Alex has access to this!`)
@@ -72,9 +72,8 @@ bot.on('messageCreate', async message => {
                 }
             }
 
-            if(message.author.id == '705014771694567475' || '514720382779916318'){} else {
+            if(message.author.id != '514720382779916318'){
                 if(command.inProgress){
-                    if(message.author.id != '514720382779916318'){
                         const inProgress = new Discord.MessageEmbed()
                         .setAuthor(bot.user.username, bot.user.displayAvatarURL())
                         .addField('WIP!', 'This command is still in progress.')
@@ -82,7 +81,6 @@ bot.on('messageCreate', async message => {
                         .setColor('FF0000')
                         message.channel.send({ embeds: [ inProgress] })
                         return
-                    }
                 }
 
                 if(command.permission){
