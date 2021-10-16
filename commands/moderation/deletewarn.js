357c5578a8d755d6d7fb2148906c2339e19dea81
AlexxScripts <78374927+AlexxScripts@users.noreply.github.com>
2021-10-16 13:48:15 +0800

    Create deletewarn.js

---
+++ commands/moderation/deletewarn.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const { version, prefix } = require('../../config.json')
+var num = 0
+
+module.exports = {
+    name: 'deletewarn',
+    usage: `${prefix}deletewarn [user] [id]`,
+    aliases: ['delwarn'],
+    inProgress: true,
+    involvesModeration: false,
+    permission: 'MANAGE_MESSAGES',
+    category: 'MODERATION',
+    cooldown: 0,
+    description: 'Deletes a warning from a user.',
+
+    async run(bot, message, args){
+        num++
+    }
+}
